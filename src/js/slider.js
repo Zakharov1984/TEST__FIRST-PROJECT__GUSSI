866565d58592cebcab340fbd7e78f22045073760
Zakharov <zakharov.products@gmail.com>
2022-02-18 22:53:34 +0300

    added_script_for_slider

---
+++ src/js/slider.js
@@ -0,0 +1,39 @@
+let leftArrowElement = document.querySelector('.arrow-left'),
+rightArrowElement = document.querySelector('.arrow-right'),
+sliderTotalElement = document.querySelector('.slider__total');
+
+
+function Slider(number, id, title, description) {
+    this.number = number;
+    this.id = id;
+    this.title = title;
+    this.description = description;
+}
+
+let sliderArray = [
+    new Slider(1, 0, 'Light coat' ,'More cotton, more freedom'),
+    new Slider(2, 1, 'Sandy longsleeve', 'More cotton, more freedom'),
+    new Slider(3, 2, 'Lime jacket', 'More cotton, more freedom'),
+]
+
+sliderTotalElement.querySelector('span').innerHTML = sliderArray.length;
+
+leftArrowElement.addEventListener('click', event => {
+    if (document.querySelector('.slider__item_active').id !== 1) {
+        let nexElement = document.querySelector(`[id="${document.querySelector('.slider__item_active').id - 1}"]`);
+        nexElement.classList.add('slider__item_active');
+        nexElement.nextSibling.classList.remove('slider__item_active');
+    } else {
+        event.preventDefault();
+    }
+})
+
+rightArrowElement.addEventListener('click', event => {
+    if (document.querySelector('.slider__item_active').id !== 3) {
+        let nexElement = document.querySelector(`[id="${Number(document.querySelector('.slider__item_active').id) + 1}"]`);
+        nexElement.classList.add('slider__item_active');
+        nexElement.previousSibling.classList.remove('slider__item_active');
+    } else {
+        event.preventDefault();
+    }
+})
