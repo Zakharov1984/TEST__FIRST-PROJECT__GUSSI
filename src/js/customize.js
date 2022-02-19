069de384c2282187a9ec8090948f404948c7e21b
Zakharov <zakharov.products@gmail.com>
2022-02-20 00:35:23 +0300

    added_block_size_and_script_for_size

--- src/js/customize.js
+++ src/js/customize.js
@@ -1,4 +1,6 @@
 let colorElements = document.querySelectorAll('.customize__color-item');
+let sizeItemElements = document.querySelectorAll('.customize__size-item');
+let sizeSubItemElements = document.querySelectorAll('.customize__size-subitem');
 
 function customizeColor() {
     colorElements.forEach(element => {
@@ -14,3 +16,22 @@ function customizeColor() {
 }
 
 customizeColor();
+
+function customizeSize() {
+    sizeItemElements.forEach(element => {
+        element.addEventListener('click', event => {
+            if (!event.currentTarget.classList.contains('customize__size-item_active')) {
+                if (!event.currentTarget.classList.contains('customize__size-item_no-product')) { // еще одна проверка, чтобы исключить размер которого нет.
+                    sizeItemElements.forEach(element => {
+                        element.classList.remove('customize__size-item_active');
+                        element.firstChild.classList.remove('customize__size-subitem_active');
+                    })
+                    event.currentTarget.classList.add('customize__size-item_active');
+                    event.currentTarget.firstChild.classList.add('customize__size-subitem_active');
+                }
+            }
+        })
+    })
+}
+
+customizeSize();
